b4ecfffb63fccfbe562b125fd4c48f64afeb3c5c
Anand T <anand.thankappan.ctr@ivueit.com>
2025-10-27 11:26:52 +0530

    arranged code in packages

--- menu.go
+++ menu.go
@@ -2,34 +2,34 @@ package main
 
 // TODO: Would like to know why StartPaymentGateway was not able to invoked from main.go
 
-// import (
-// 	"fmt"
-// 	paymentGateway "payment_lab/PaymentGateway"
-// )
+import (
+	"fmt"
+	paymentGateway "payment_lab/PaymentGateway"
+)
 
-// func StartPaymentGateway(gatewayAccount *paymentGateway.GatewayAccount) {
-// 	for {
-// 		displayMenu()
-// 		var choice int
-// 		fmt.Scanln(&choice)
-// 		switch choice {
-// 		case 1:
-// 			gatewayAccount.InitializeTransaction()
-// 		case 2:
-// 			transactionID := paymentGateway.ReadTransactionID()
-// 			gatewayAccount.RequestRefund(transactionID)
-// 		case 3:
-// 			gatewayAccount.ViewTransactionHistory()
-// 		case 4:
-// 			return
-// 		}
-// 	}
-// }
+func StartPaymentGateway(gatewayAccount *paymentGateway.GatewayAccount) {
+	for {
+		displayMenu()
+		var choice int
+		fmt.Scanln(&choice)
+		switch choice {
+		case 1:
+			gatewayAccount.InitializeTransaction()
+		case 2:
+			transactionID := paymentGateway.ReadTransactionID()
+			gatewayAccount.RequestRefund(transactionID)
+		case 3:
+			gatewayAccount.ViewTransactionHistory()
+		case 4:
+			return
+		}
+	}
+}
 
-// func displayMenu() {
-// 	fmt.Println("\n\n\n------ Menu --------")
-// 	fmt.Println("1. Make a payment")
-// 	fmt.Println("2. Request a refund")
-// 	fmt.Println("3. View transaction history")
-// 	fmt.Println("4. Exit")
-// }
+func displayMenu() {
+	fmt.Println("\n\n\n------ Menu --------")
+	fmt.Println("1. Make a payment")
+	fmt.Println("2. Request a refund")
+	fmt.Println("3. View transaction history")
+	fmt.Println("4. Exit")
+}
